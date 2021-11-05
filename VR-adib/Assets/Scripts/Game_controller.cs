fea90b0d29c8cea3807fcbfdf6ddc9fbd69982a5
Trinebjorg <trini20@student.sdu.dk>
2021-11-05 13:37:08 +0100

    Memory game 0.1

---
+++ VR-adib/Assets/Scripts/Game_controller.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Game_controller : MonoBehaviour
+{
+    public List<Button> btns = new List<Button>();
+
+    void Start()
+    {
+        GetButtons();
+
+    }
+    
+     void GetButtons()
+     {
+            GameObject[] objects = GameObject.FindGameObjectsWithTag ("PuzzleButton");
+
+            for(int i = 0; i < objects.Length; i++)
+            {
+                btns.Add(objects[i].GetComponent<Button>());
+            }
+
+        
+    }
+}
